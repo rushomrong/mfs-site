134a789d176ad091861ddcd26429f9a897ce1dae
rushomrong <rushomarak@gmail.com>
2024-09-20 01:51:17 +0600

    MFS site updated

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,12 +1,23 @@
 //initialized the addmoney button
 document
-  .getElementById("money-add")
+  .getElementById("money-add-btn")
   .addEventListener("click", function (event) {
     event.preventDefault();
 
     //Getting amount data from the shared data
-    const addMoney = getInputFieldById("input-amount");
-    console.log("Amount added", addMoney);
-    const addPin = getInputFieldById("input-pin");
-    console.log("Pin added", addPin);
+    const addMoney = getInputFieldValueById("input-amount");
+    const addPin = getInputFieldValueById("input-pin");
+
+    if (addPin === 1234) {
+      //calling the account balance id
+      const balance = getTextFieldValueById("total-balance");
+      const newBalance = balance + addMoney;
+      console.log(newBalance);
+
+      //add the balance to the total balance field
+      document.getElementById("total-balance").innerText = newBalance;
+      window.alert("Money added successfully");
+    } else {
+      window.alert("Wrong Pin Number!");
+    }
   });
--- js/utilities.js
+++ js/utilities.js
@@ -1,5 +1,16 @@
 //shared js functions
-function getInputFieldById(id) {
+//getting the input value id
+function getInputFieldValueById(id) {
+  //storing the input data
   const inputMoney = document.getElementById(id).value;
-  return inputMoney;
+  const inputNumber = parseFloat(inputMoney);
+  return inputNumber;
+}
+
+//checking the available balance
+
+function getTextFieldValueById(id) {
+  const inputText = document.getElementById(id).innerText;
+  const textValue = parseFloat(inputText);
+  return textValue;
 }
